c7a9e9e629d3baa459d9e415e36804f1e14e7a8a
Bobo-BKL <75877387+Bobo-BKL@users.noreply.github.com>
2021-01-11 20:03:38 +0900

    Update 03_왕실의나이트.py
    if 문법 교정

--- Ch04_Implementation/03_왕실의나이트.py
+++ Ch04_Implementation/03_왕실의나이트.py
@@ -1,15 +1,18 @@
 data = input()
-row = int(data[1]) - 1
-col = int(ord(data[0])) - int(ord('a'))
+row = int(data[1])
+col = int(ord(data[0])) - int(ord('a')) + 1
 
-steps = [ (-2, -1), (-1, -2), (1, -2), (-2, 1), (-1, 2), (2, -1), (1, 2), (2, 1) ]
+steps = [
+			(-2, -1), (-1, -2), (1, -2), (-2, 1),
+			(2, 1), (1, 2), (-1, 2), (2, -1)
+]
 
 result = 0
-for step in steps:
-    tmp_row = row + step[0]
-    tmp_col = col + step[1]
 
-    if tmp_row >= 0 and tmp_row < 8 and tmp_col >= 0 and tmp_col < 8:
-        result += 1
+for step in steps:
+	next_r = step[0] + row
+	next_c = step[1] + col
+	
+	if 0 < next_r < 9 and 0 < next_c < 9: result += 1
 
 print(result)
