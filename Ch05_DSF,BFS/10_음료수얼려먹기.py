3cb018a5cc208e11718b0e845d2f0b996ba30678
Bobo-BKL <75877387+Bobo-BKL@users.noreply.github.com>
2021-01-13 22:47:11 +0900

    Update 10_음료수얼려먹기.py
    코드 최적화

--- Ch05_DSF,BFS/10_음료수얼려먹기.py
+++ Ch05_DSF,BFS/10_음료수얼려먹기.py
@@ -3,26 +3,26 @@ n, m = map(int, input().split())
 data = []
 for _ in range(n):
     data.append(list(map(int, input())))
- 
-def dfs(x, y):
-    if x < 0 or x >= n or y < 0 or y >= m:
-        return False
     
-    if data[x][y] == 0:
-        data[x][y] = 1
-        dfs(x-1, y)
-        dfs(x+1, y)
-        dfs(x, y-1)
-        dfs(x, y+1)
-        return True
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+def dfs(x, y):
+    data[x][y] = 1
+    
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
         
-    return False
-
+        if nx < 0 or nx >= n or ny < 0 or ny >= m: continue
+        
+        if data[nx][ny] == 0: dfs(nx, ny)
+        
 result = 0
-
-for x in range(n):
-    for y in range(m):
-        if dfs(x, y) == True:
+for i in range(n):
+    for j in range(m):
+        if data[i][j] == 0: 
+            dfs(i, j)
             result += 1
-
+            
 print(result)
