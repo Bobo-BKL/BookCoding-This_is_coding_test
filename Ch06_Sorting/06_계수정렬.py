f68d5b6deba8836e8ad8e75083c9e0ce73e9a9fb
Bobo-BKL <75877387+Bobo-BKL@users.noreply.github.com>
2021-01-16 21:43:17 +0900

    Update 06_계수정렬.py

--- Ch06_Sorting/06_계수정렬.py
+++ Ch06_Sorting/06_계수정렬.py
@@ -1,10 +1,8 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
 
-count = [0] * (max(array) + 1)
+d = [0] * (max(array) + 1)
 
 for i in range(len(array)):
-    count[array[i]] += 1
+    d[array[i]] = 1
     
-for i in range(len(count)):
-    for j in range(count[i]):
-        print(i, end = ' ')
+print([x for x in range(len(d)) if d[x] == 1])
