8db631165d9b7f2da331c9628819bc26ad1e101f
Bobo-BKL <75877387+Bobo-BKL@users.noreply.github.com>
2021-01-16 23:30:51 +0900

    Update 08_떡볶이떡만들기.py
    코드 최적화

--- Ch07_Binary Search/08_떡볶이떡만들기.py
+++ Ch07_Binary Search/08_떡볶이떡만들기.py
@@ -11,6 +11,7 @@ while start <= end:
     sum = 0
     for i in data:
         if i > mid: sum += i - mid
+    # summ = sum([(x - mid) for x in lenpro if mid < x])
     
     if sum < m: end = mid - 1
     else:
