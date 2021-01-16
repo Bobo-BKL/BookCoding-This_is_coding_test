f709fc34a10ca5fc079d13e91821239cc606e4f5
Bobo-BKL <75877387+Bobo-BKL@users.noreply.github.com>
2021-01-16 21:49:30 +0900

    Update 06_계수정렬.py
    코드 최적화

--- Ch06_Sorting/06_계수정렬.py
+++ Ch06_Sorting/06_계수정렬.py
@@ -3,6 +3,6 @@ array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
 d = [0] * (max(array) + 1)
 
 for i in range(len(array)):
-    d[array[i]] = 1
+    d[array[i]] += 1
     
-print([x for x in range(len(d)) if d[x] == 1])
+print([x for x in range(len(d)) for _ in range(d[x])])
