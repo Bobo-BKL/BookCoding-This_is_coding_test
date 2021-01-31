044ab679442ead0666861aac65316c8832194979
Bobo-BKL <75877387+Bobo-BKL@users.noreply.github.com>
2021-01-31 19:45:12 +0900

    Update 10_자물쇠와열쇠.py
    코드 에러 수정

--- Ch11_알고리즘 기출문제/10_자물쇠와열쇠.py
+++ Ch11_알고리즘 기출문제/10_자물쇠와열쇠.py
@@ -1,7 +1,5 @@
-import copy
-
-key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
-lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+key = [[0, 0, 0, 0], [1, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0]]
+lock = [[1, 1, 1, 1, 1, 1], [1, 1, 0, 1, 1, 0], [1, 0, 1, 1, 0, 1], [1, 1, 1, 1, 1, 1], [1, 1, 0, 1, 1, 0], [1, 0, 1, 1, 0, 1]]
 
 def rotate_90d_clockwise(key):
     n = len(key)
@@ -10,39 +8,43 @@ def rotate_90d_clockwise(key):
     
     for i in range(n):
         for j in range(m):
-            result[j][n - i -1] = key[i][j]
+            result[j][n - i - 1] = key[i][j]
             
     return result
 
-def is_unlock(key_length, lock):
-    for i in range(key_length):
-        for j in range(key_length):
-            if lock[key_length + i][key_length + j] != 1: return False
+def is_unlock(lock_length, key_len, lock):
+    for i in range(lock_length):
+        for j in range(lock_length):
+            if lock[(key_len - 1) + i][(key_len -1) + j] != 1: 
+                return False
             
     return True
-            
 
 def solution(key, lock):
-    m = len(key)
-    n = len(lock)
+    key_len = len(key)
+    lock_len = len(lock)
     
-    new_lock = [[0] * (3 * n) for _ in range(3 * n)]
-    for i in range(n):
-        for j in range(n):
-            new_lock[3 + i][3 + j] = lock[i][j]
+    new_lock_len = lock_len + (key_len - 1) * 2
+    new_lock = [[0] * new_lock_len for _ in range(new_lock_len)]
+    for i in range(lock_len):
+        for j in range(lock_len):
+            new_lock[(key_len - 1) + i][(key_len - 1) + j] = lock[i][j]
             
     for rotate in range(4):
         key = rotate_90d_clockwise(key)
         
-        for i in range(n * 2):
-            for j in range(n * 2):
-                copy_mat = copy.deepcopy(new_lock)
+        for i in range(new_lock_len - (key_len - 1)):
+            for j in range(new_lock_len - (key_len - 1)):
                 
-                for k in range(n):
-                    for l in range(n):
-                        copy_mat[i + k][j + l] += key[k][l]
+                for k in range(key_len):
+                    for l in range(key_len):
+                        new_lock[i + k][j + l] += key[k][l]
                         
-                if is_unlock(m, copy_mat): return True
+                if is_unlock(lock_len, key_len, new_lock): return True
+                
+                for k in range(key_len):
+                    for l in range(key_len):
+                        new_lock[i + k][j + l] -= key[k][l]
                 
     return False
     
