2111d6e844ddf87c23ea6f12fc422fe10495b077
Bobo-BKL <75877387+Bobo-BKL@users.noreply.github.com>
2021-02-03 17:42:25 +0900

    Update 14_외벽점검.py
    버그 교정

--- Ch11_알고리즘 기출문제/14_외벽점검.py
+++ Ch11_알고리즘 기출문제/14_외벽점검.py
@@ -1,54 +1,29 @@
+from itertools import permutations
+
 def solution(n, weak, dist):
-    answer = 0
-    weak.sort()
-    dist.sort(reverse = True)
+    length = len(weak)
     
-    for person in dist:
-        if not weak: break
-        
-        max_sum = 0
-        max_list = []
-        max_far = 0
-        for now in weak:
-            r_list = []
-            l_list = []
-            r_sum = 0
-            l_sum = 0
-            # 시계방향
-            for i in range(now, now + person + 1):
-                i = i % n
-                if i in weak:
-                    r_sum += 1
-                    r_list.append(i)
-                    
-            # 반시계방향
-            for i in range(now - person, now + 1):
-                if i < 0: i = n - i
-                if i in weak:
-                    l_sum += 1
-                    l_list.append(i)
+    for i in range(length): weak.append(weak[i] + n)
+    answer = len(dist) + 1
+    
+    for start in range(length):
+        for friends in list(permutations(dist, len(dist))):
+            count = 1
+            position = weak[start] + friends[count - 1]
             
-            if r_sum < l_sum:
-                if max_sum < l_sum:
-                    max_list = l_list
-                    max_sum = l_sum
-            elif r_sum > l_sum:
-                if max_sum < r_sum:
-                    max_list = r_list
-                    max_sum = r_sum
+            for index in range(start, start + length):
+                if position < weak[index]:
+                    count += 1
+                    if count > len(dist): break
+                    position = weak[index] + friends[count - 1]
+            answer = min(answer, count)
             
-        answer += 1
-        for x in max_list: weak.remove(x)
-        print(max_list)
-    if weak: answer = -1
+    if answer > len(dist): return -1
     
-    return answer
-    
+    return answer        
+
 n = 12
 weak = [1, 5, 6, 10]
 dist = [1, 2, 3, 4]
-print(solution(n, weak, dist))
 
-weak = [1, 3, 4, 9, 10]
-dist = [3, 5, 7]
-print(solution(n, weak, dist))
+print(solution(n, weak, dist))            
