d2c9df6b57e8685a302478e3bd695db3aaf8a842
Bobo-BKL <75877387+Bobo-BKL@users.noreply.github.com>
2021-04-23 19:54:27 +0900

    Update (unsolved)큐빙.py

--- 추가_삼성 역량테스트 기출문제/(unsolved)큐빙.py
+++ 추가_삼성 역량테스트 기출문제/(unsolved)큐빙.py
@@ -1,129 +1,72 @@
-# 해당 코드의 문제
-# 면 자체의 고정된 방향때문에 각 상황에서의 line 대입 시 장애가 생김
-
+import copy
 colors = ['w', 'y', 'r', 'o', 'g', 'b']
-faces = ['U', 'D', 'F', 'B', 'L', 'R']
-directions = ['+', '-']
-turns = [
-    # [딸린 4개의 면번호 시계 방향순]
-    [2, 4, 3, 5],
-    [2, 5, 3, 4],
-    [0, 5, 1, 4],
-    [0, 4, 1, 5],
-    [0, 2, 1, 3],
-    [0, 3, 1, 2]
-]
-match_rowcol = [
-    # [행0인가 열1인가, 그것의 번호(, 인덱싱 방향?)]
-    [[0, 0], [0, 0], [0, 0], [0, 0]],
-    [[0, 2], [0, 2], [0, 2], [0, 2]],
-    [[0, 2], [1, 0], [0, 2], [1, 2]],
-    [[0, 0], [1, 0], [0, 0], [1, 2]],
-    [[1, 0], [1, 0], [1, 0], [1, 2]],
-    [[1, 2], [1, 0], [1, 2], [1, 2]]
+directions = ['U', 'D', 'F', 'B', 'L', 'R']
+n, m = 6, 3
+
+turn_points = [  # face_turn, face_num
+    [(0, 0, 0, 0), (2, 4, 3, 5)],
+    [(2, 2, 2, 2), (2, 5, 3, 4)],
+    [(2, 3, 2, 1), (1, 4, 0, 5)],
+    [(0, 3, 0, 1), (1, 5, 0, 4)],
+    [(3, 3, 1, 1), (1, 3, 0, 2)],
+    [(1, 3, 3, 1), (1, 2, 0, 3)]
 ]
 
-
-# 해당면을 돌려
-def turn_current_face(top_face, di):
-    temp = [[0] * 3 for _ in range(3)]
-    if di == 0:  # 시계방향
-        for i in range(3):
-            for j in range(3):
-                temp[j][3 - i - 1] = top_face[i][j]
-    else:  # 반시계방향
-        for j in range(3):
-            for i in range(3):
-                temp[i][j] = top_face[j][3 - i - 1]
-    return temp
+t = int(input())
 
 
-def get_col(fa, num):
-    return [fa[i][num] for i in range(3)]
+def turn_face(face, turn_cnt):
+    result = copy.deepcopy(face)
+    for _ in range(turn_cnt):
+        result = list(zip(*result[::-1]))
+    return result
 
 
-def get_row(fa, num):
-    return fa[num]
+def turn_cube(cube, oface, odir):
+    # 기준면 일단 회전!
+    cube[oface] = turn_face(cube[oface], odir)
+
+    face_turn_cnts, faces = turn_points[oface]
+    # 기준면 중심으로 네 개의 면 복사
+    temp = [[] for _ in range(m)]
+    for cnt, face in zip(face_turn_cnts, faces):
+        temp_add = turn_face(cube[face], cnt)
+        for i in range(m):
+            temp[i].extend(temp_add[i])
+
+    # 횟수대로 돌려돌려!
+    for _ in range(odir):
+        s = temp[0][-3:]
+        for k in range(3, 0, -1):
+            i = m * k - 1
+            temp[0][i + 1:i + 4] = temp[0][i - 2:i + 1]
+        temp[0][:3] = s
+
+    # 붙여넣기
+    col = 0
+    for cnt, face in zip(face_turn_cnts, faces):
+        temp_add = turn_face([temp[i][col:col + m] for i in range(m)], 4 - cnt)
+        for i in range(m):
+            cube[face][i] = temp_add[i]
+        col += 3
 
 
-def get_rowcol(is_rowcol, turn_face, num):
-    if is_rowcol == 0:  #행
-        return get_row(turn_face, num)
-    else:
-        return get_col(turn_face, num)
+def print_cube_topface(topface):
+    for i in range(m):
+        print(''.join([colors[element] for element in topface[i]]))
 
 
-# !!!!!고칠거면 여길 좀 어떻게 고쳐봐!!!!!
-# line의 인덱스랑 면의 인덱스 방향이 일치한지 또 생각해줘야함
-def change_one_line(is_rowcol, face_a, line, num):
-    if is_rowcol == 0:  # 행
-        face_a[num] = line
-    else:  # 열
-        for i in range(3):
-            face_a[i][num] = line[i]
-    return face_a
+def get_result():
+    # test case
+    for _ in range(t):
+        cube = [[[i] * m for _ in range(m)] for i in range(n)]
+        k = int(input())
+        cases = list(map(lambda x: (directions.index(x[0]), 3 if x[1] == '-' else 1), input().split()))
+
+        for face_num, dir in cases:
+            turn_cube(cube, face_num, dir)
+
+        print_cube_topface(cube[0])
 
 
-def turn_clockwise_affected_face(cu, fa):
-    faces_to_turn = turns[fa]
-
-    saved = get_rowcol(match_rowcol[fa][3][0], cu[faces_to_turn[3]], match_rowcol[fa][3][1])
-    for f in range(3, 0, -1):
-        temp = get_rowcol(match_rowcol[fa][f - 1][0], cu[faces_to_turn[f - 1]], match_rowcol[fa][f - 1][1])
-        cu[faces_to_turn[f]] = change_one_line(match_rowcol[fa][f][0], cu[faces_to_turn[f]], temp, match_rowcol[fa][f][1])
-
-    cu[faces_to_turn[0]] = change_one_line(match_rowcol[fa][0][0], cu[faces_to_turn[0]], saved, match_rowcol[fa][0][1])
-    return cu
-
-
-def turn_counterclockwise_affected_face(cu, fa):
-    faces_to_turn = turns[fa]
-
-    saved = get_rowcol(match_rowcol[fa][0][0], cu[faces_to_turn[0]], match_rowcol[fa][0][1])
-    for f in range(1, 4):
-        temp = get_rowcol(match_rowcol[fa][f][0], cu[faces_to_turn[f]], match_rowcol[fa][f][1])
-        cu[faces_to_turn[f - 1]] = change_one_line(match_rowcol[fa][f - 1][0], cu[faces_to_turn[f - 1]], temp, match_rowcol[fa][f - 1][1])
-
-    cu[faces_to_turn[3]] = change_one_line(match_rowcol[fa][3][0], cu[faces_to_turn[3]], saved, match_rowcol[fa][3][1])
-    return cu
-
-
-# 해당면에 딸린 네놈 돌려
-def turn_all_affected_face(cu, fa, di):
-    if di == 0:  # 시계방향
-        cu = turn_clockwise_affected_face(cu, fa)
-    else:  # 반시계방향
-        cu = turn_counterclockwise_affected_face(cu, fa)
-    return cu
-
-
-def print_top_face(top_face):
-    for i in range(3):
-        temp = []
-        for j in range(3):
-            temp.append(colors[top_face[i][j]])
-        print(''.join(temp))
-
-
-T = int(input())
-for _ in range(T):
-    # init cube
-    cube = [[] for _ in range(6)]
-    for c in range(6):
-        for _ in range(3):
-            cube[c].append([c] * 3)
-
-    n = int(input())
-    data = list(input().split())
-
-    for c in range(n):
-        face, direct = data[c]
-        face = faces.index(face)
-        direct = directions.index(direct)
-
-        # main
-        cube[face] = turn_current_face(cube[face], direct)
-        cube = turn_all_affected_face(cube, face, direct)
-
-    print_top_face(cube[0])
-
+get_result()
